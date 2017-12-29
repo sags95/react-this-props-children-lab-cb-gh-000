7f66e50b34c5853d993d4e5a23e4ba7ecd7dbbbb
Alex Sagel <alex.sagel7587@gmail.com>
2017-12-29 19:01:59 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,16 @@
 // Code Invitation Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Invitation extends React.Component{
+  render(){
+    return(
+      <div className="wrapper">
+        <h1>You've been invited!</h1>
+        <div className="invite_body">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,20 @@
 // Code ThemedDecoration Component Here
+import React from 'react';
+
+export default class ThemedDecorations extends React.Component{
+  render(){
+
+    const extraProp = React.Children.map(
+      this.props.children, child => {
+        return React.cloneElement(child, {
+          className: child.props.title
+        })
+      }
+    )
+    return(
+      <div className="wrapper">
+        {extraProp}
+      </div>
+    );
+  }
+}
